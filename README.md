91b1076b9fb5594b29551465ee874c201d33945a
bclswl0827 <bclswl0827@users.noreply.github.com>
2025-12-24 05:31:22 +0000

    deploy: a08c8eb360b304b5e95ab01611ee3a0c4002cccb

--- README.md
+++
@@ -1,3 +0,0 @@
-# spectrogram-js
-
-🎶 A simple and intuitive TypeScript library for generating spectrogram waterfall plots.
--- src/index.ts
+++
@@ -1,3 +0,0 @@
-export type { SpectrogramConfig, SpectrogramData } from './core/SpectrogramModel';
-export type { ColorMapName } from './render/ColorMap';
-export { Spectrogram } from './spectrogram';
